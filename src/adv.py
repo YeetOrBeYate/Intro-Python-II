7a55d11fb75f09e7e122f78ad461f86ed0d7a6fe
Kyle Corbin <kylecorbin26@gmail.com>
2020-03-11 17:40:16 -0400

    making the first pass

--- src/adv.py
+++ src/adv.py
@@ -44,6 +44,16 @@ room['treasure'].s_to = room['narrow']
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+user = int(input("[1] Start!  [2] Help     [9] Quit\n"))
+
+while not user == 9:
+
+    if user ==1:
+        str(input(""))
+
+
+
+
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
